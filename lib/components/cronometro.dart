81ab9e3afba46745be9eda52f513f8cdcb046067
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2024-01-22 09:26:51 -0300

    460. Componente Botão Cronômetro

--- lib/components/cronometro.dart
+++ lib/components/cronometro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/components/cronometro_botao.dart';
 
 class Cronometro extends StatelessWidget {
   const Cronometro({super.key});
@@ -12,14 +13,45 @@ class Cronometro extends StatelessWidget {
         children: [
           Text(
             'Hora de Trabalhar',
-            style: TextStyle(fontSize: 40, color: Colors.white),
+            style: TextStyle(
+              fontSize: 40,
+              color: Colors.white,
+            ),
           ),
-          SizedBox(
-            height: 20,
-          ),
+          SizedBox(height: 20),
           Text(
             '25:00',
-            style: TextStyle(fontSize: 120, color: Colors.white),
+            style: TextStyle(
+              fontSize: 120,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: CronometroBotao(
+                  texto: 'Iniciar',
+                  icone: Icons.play_arrow,
+                ),
+              ),
+              // Padding(
+              //   padding: EdgeInsets.only(right: 10),
+              //   child: CronometroBotao(
+              //     texto: 'Parar',
+              //     icone: Icons.stop,
+              //   ),
+              // ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: CronometroBotao(
+                  texto: 'Reiniciar',
+                  icone: Icons.refresh,
+                ),
+              ),
+            ],
           )
         ],
       ),
---
+++ lib/components/cronometro_botao.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class CronometroBotao extends StatelessWidget {
+  final String texto;
+  final IconData icone;
+  const CronometroBotao({
+    Key? key,
+    required this.texto,
+    required this.icone,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 30,
+            vertical: 20,
+          ),
+          textStyle: const TextStyle(fontSize: 25)),
+      onPressed: () {},
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 5),
+            child: Icon(
+              icone,
+              size: 35,
+            ),
+          ),
+          Text(texto),
+        ],
+      ),
+    );
+  }
+}
