72485fb6edc7f7d85523af60c10b369743ab4d1e
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2024-01-23 08:16:00 -0300

    465. Pomodoro Store #04

--- lib/components/cronometro.dart
+++ lib/components/cronometro.dart
@@ -11,30 +11,32 @@ class Cronometro extends StatelessWidget {
   Widget build(BuildContext context) {
     final store = Provider.of<PomodoroStore>(context);
 
-    return Container(
-      color: store.estaTrabalhando() ? Colors.red : Colors.green,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            store.estaTrabalhando() ? 'Hora de Trabalhar' : 'Hora de descansar',
-            style: const TextStyle(
-              fontSize: 40,
-              color: Colors.white,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            '${store.minutos.toString().padLeft(2, '0')}:${store.segundos.toString().padLeft(2, '0')}',
-            style: const TextStyle(
-              fontSize: 120,
-              color: Colors.white,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Observer(
-            builder: (_) {
-              return Row(
+    return Observer(
+      builder: (_) {
+        return Container(
+          color: store.estaTrabalhando() ? Colors.red : Colors.green,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                store.estaTrabalhando()
+                    ? 'Hora de Trabalhar'
+                    : 'Hora de descansar',
+                style: const TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                '${store.minutos.toString().padLeft(2, '0')}:${store.segundos.toString().padLeft(2, '0')}',
+                style: const TextStyle(
+                  fontSize: 120,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Visibility(
@@ -65,11 +67,11 @@ class Cronometro extends StatelessWidget {
                     ),
                   ),
                 ],
-              );
-            },
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/store/pomodoro.store.dart
+++ lib/store/pomodoro.store.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:mobx/mobx.dart';
 
 part 'pomodoro.store.g.dart';
@@ -23,21 +25,35 @@ abstract class _PomodoroStore with Store {
   int tempoDescanso = 1;
 
   @observable
-  TipoIntervalo tipoIntervalo = TipoIntervalo.descanso;
+  TipoIntervalo tipoIntervalo = TipoIntervalo.trabalho;
+
+  Timer? cronometro;
 
   @action
   void iniciar() {
     iniciado = true;
+    cronometro = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+      if (minutos == 0 && segundos == 0) {
+        _trocarTipoIntervalo();
+      } else if (segundos == 0) {
+        segundos = 59;
+        minutos--;
+      } else {
+        segundos--;
+      }
+    });
   }
 
   @action
   void reiniciar() {
-    iniciado = false;
+    // iniciado = false;
+    parar();
   }
 
   @action
   void parar() {
     iniciado = false;
+    cronometro?.cancel();
   }
 
   @action
@@ -65,5 +81,16 @@ abstract class _PomodoroStore with Store {
 
   bool estaDescansando() {
     return tipoIntervalo == TipoIntervalo.descanso;
+  }
+
+  void _trocarTipoIntervalo() {
+    if (estaTrabalhando()) {
+      tipoIntervalo = TipoIntervalo.descanso;
+      minutos = tempoDescanso;
+    } else {
+      tipoIntervalo = TipoIntervalo.trabalho;
+      minutos = tempoTrabalho;
+    }
+    segundos = 0;
   }
 }
